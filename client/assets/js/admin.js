99a2b65284b454b9eca4e4b71325bafa964ce917
Wu Donghui <woo.donghui@gmail.com>
2017-06-26 19:13:41 +0800

    number input

--- client/assets/js/admin.js
+++ client/assets/js/admin.js
@@ -2,41 +2,46 @@
 
 (function() {
 
-	var clock = document.querySelector('digiclock');
-	
-	// But there is a little problem
-	// we need to pad 0-9 with an extra
-	// 0 on the left for hours, seconds, minutes
-	
-	var pad = function(x) {
-		return x < 10 ? '0'+x : x;
-	};
-	
-	var ticktock = function() {
-		var d = new Date();
-		
-		var h = pad( d.getHours() );
-		var m = pad( d.getMinutes() );
-		var s = pad( d.getSeconds() );
-		
-		var current_time = d.toLocaleDateString() + ' '+ [h,m,s].join(':');
-		//var current_time = d.toLocaleDateString();
-		
-		clock.innerHTML = current_time;
-		
-	};
-	
-	ticktock();
-	
-	// Calling ticktock() every 1 second
-	setInterval(ticktock, 1000);
-	
+    // var clock = document.querySelector('digiclock');
+
+// // But there is a little problem
+// // we need to pad 0-9 with an extra
+// // 0 on the left for hours, seconds, minutes
+
+// var pad = function(x) {
+//     return x < 10 ? '0' + x : x;
+// };
+
+// var ticktock = function() {
+//     var d = new Date();
+
+//     var h = pad(d.getHours());
+//     var m = pad(d.getMinutes());
+//     var s = pad(d.getSeconds());
+
+//     var current_time = d.toLocaleDateString() + ' ' + [h, m, s].join(':');
+//     //var current_time = d.toLocaleDateString();
+
+//     clock.innerHTML = current_time;
+
+// };
+
+// ticktock();
+
+// // Calling ticktock() every 1 second
+// setInterval(ticktock, 1000);
+
 }());
 
 /* ---------- Notifications ---------- */
-	$('.noty').click(function(e){
-		e.preventDefault();
-		var options = $.parseJSON($(this).attr('data-noty-options'));
-		noty(options);
-	});
+// $('.noty').click(function(e) {
+//     e.preventDefault();
+//     var options = $.parseJSON($(this).attr('data-noty-options'));
+//     noty(options);
+// });
 
+$(document).ready(function() {
+    $("input[type='text']").click(function() {
+        $(this).select();
+    });
+});
